ca95a8659412c403357242465b071035a9703b71
Rasmus Tibell <rasmus.tibell@ferrologic.se>
2018-09-06 16:38:39 +0200

    added camel to pom

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -1,11 +1,21 @@
 package bootwildfly;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWildFlyController {
 
+    @Autowired
+    ProducerTemplate producerTemplate;
+
+    @RequestMapping("direct")
+    public String direct(){
+        producerTemplate.sendBody("direct:firstRoute", "Hello, SpringBoot on Wildfly via camel route");
+    }
 
     @RequestMapping("hello")
     public String sayHello(){
