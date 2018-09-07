1e3d317175df89bf11115d2bbb0f69b1f99fd6c4
Rasmus Tibell <rasmus.tibell@ferrologic.se>
2018-09-07 13:14:03 +0200

    Adjust route

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -12,21 +12,32 @@ import org.springframework.context.annotation.Configuration;
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackage = "bootwildfly")
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-  @Bean
-  public ServletRegistrationBean camelServletRegistrationBean() {
-    ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/camel/*");
-    registration.setName("CamelServlet");
-    return registration;
-  }
+    @Bean
+    public ServletRegistrationBean camelServletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/camel/*");
+        registration.setName("CamelServlet");
+        return registration;
+    }
+  
+    @Bean
+    public RouteBuilder routeBuilder() {
+        return new CamelRoutes();
+    }
+
 //    @Override
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //        return application.sources(Application.class);
